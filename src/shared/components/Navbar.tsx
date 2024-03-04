3fb7665adf62ae32ad5dfa1881e16d10e6c39cf6
H1V35 <higesjorge@gmail.com>
2024-03-04 18:36:50 +0100

    🎨 style: Apply BEM methodology for styles; Modify Navbar structure

--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -1,21 +1,36 @@
 import { NavLink } from 'react-router-dom';
 
 const routes = [
-  { to: '/', text: 'Home' },
   { to: '/search', text: 'Search' },
   { to: '/watchlist', text: 'Watchlist' },
+  { to: '/profile', text: 'Profile' },
 ];
 
 export function NavBar() {
   return (
-    <nav>
-      <ul>
-        {routes.map(({ to, text }) => (
-          <li>
-            <NavLink to={to}>{text}</NavLink>
-          </li>
-        ))}
-      </ul>
+    <nav className="navbar">
+      <div className="navbar__container">
+        <NavLink to={'/'}>
+          <h1 className="navbar__title">
+            <span className="navbar__title--highlighted">Anim</span>Envy
+          </h1>
+        </NavLink>
+
+        <ul className="navbar__links">
+          {routes.map(({ to, text }) => (
+            <li>
+              <NavLink
+                to={to}
+                className={({ isActive }) =>
+                  isActive ? 'navbar__link navbar__link--active' : 'navbar__link'
+                }
+              >
+                {text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 }
