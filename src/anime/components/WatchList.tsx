cd1a8135fd73955bf7b978814faec227076fc019
H1V35 <higesjorge@gmail.com>
2024-03-10 20:01:21 +0100

    ✨ feat: Add WatchList component; Render Watchlist page content

---
+++ src/anime/components/WatchList.tsx
@@ -0,0 +1,20 @@
+import { AnimeItem } from './AnimeItem';
+import type { Datum } from '../interfaces/animeList';
+
+type WatchListProps = {
+  list: Datum[];
+};
+
+export function WatchList({ list }: WatchListProps) {
+  if (!list) return null;
+
+  return (
+    <div className="animeList">
+      <div className="animeList__container">
+        {list.map((anime) => (
+          <AnimeItem key={anime.mal_id} anime={anime} />
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/anime/pages/Watchlist.tsx
+++ src/anime/pages/Watchlist.tsx
@@ -1,7 +1,23 @@
+import React from 'react';
+import { WatchlistContext } from '../../providers/WatchlistProvider';
+import { WatchList } from '../components/WatchList';
+
 export function WatchlistPage() {
+  const { watchlist } = React.useContext(WatchlistContext);
+
   return (
-    <>
-      <h2>Watchlist</h2>
-    </>
+    <div className="page">
+      <div className="page__container">
+        <h2 className="page__title">Watchlist</h2>
+
+        <div className="page__list">
+          {watchlist.length > 0 ? (
+            <WatchList list={watchlist} />
+          ) : (
+            <p className="page__noContent">Nothing to see yet here...</p>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
