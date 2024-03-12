69014522d6033d4121c5eb3a44d5f3740faa4dd5
H1V35 <higesjorge@gmail.com>
2024-03-12 17:20:38 +0100

    🎨 style: Add styles to Anime page

--- src/anime/pages/Anime.tsx
+++ src/anime/pages/Anime.tsx
@@ -14,55 +14,62 @@ export function AnimePage() {
   return (
     <div className="page">
       <div className="page__container">
-        <div className="anime__above">
-          {anime.trailer.embed_url && (
-            <iframe
-              width="100%"
-              height="auto"
-              style={{ borderRadius: '8px', aspectRatio: '16/9' }}
-              src={`${anime.trailer.embed_url}&rel=0`}
-              title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              frameBorder={0}
-              className="anime__iframe"
-            ></iframe>
-          )}
-        </div>
+        <div className="anime">
+          <div className="anime__container">
+            <ToggleButton anime={anime} item="AnimeItem" />
 
-        <div className="anime__name">
-          <ToggleButton anime={anime} item="AnimeItem" />
-          <h2 className="anime__name--title">{anime.title_english}</h2>
-        </div>
+            {anime.trailer.embed_url && (
+              <iframe
+                width="100%"
+                height="auto"
+                style={{ borderRadius: '8px', aspectRatio: '16/9' }}
+                src={`${anime.trailer.embed_url}&rel=0`}
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                frameBorder={0}
+                className="anime__iframe"
+              ></iframe>
+            )}
 
-        {anime.favorites && (
-          <p className="anime__data--favorites">{roundFavorites(anime.favorites)} favorites</p>
-        )}
+            <h2 className="anime__title">{anime.title_english}</h2>
 
-        {anime.genres && (
-          <p className="anime__data--genres">
-            {anime.genres.map((genre) => genre.name).join(' • ')}
-          </p>
-        )}
+            <div className="anime__info">
+              <div className="anime__data">
+                {anime.favorites && (
+                  <p className="anime__data--favorites">
+                    {roundFavorites(anime.favorites)} favorites
+                  </p>
+                )}
 
-        {anime.synopsis && <p className="anime__data--synopsis">{anime.synopsis}</p>}
+                {anime.genres && (
+                  <p className="anime__data--genres">
+                    {anime.genres.map((genre) => genre.name).join(' • ')}
+                  </p>
+                )}
 
-        {anime.streaming && (
-          <div className="anime__streaming">
-            {anime.streaming.map((stream) => (
-              <Link to={stream.url} className="anime__streaming--link">
-                {stream.name}
-              </Link>
-            ))}
+                {anime.synopsis && <p className="anime__data--synopsis">{anime.synopsis}</p>}
+
+                {anime.streaming && (
+                  <div className="anime__data--streaming">
+                    {anime.streaming.map((stream) => (
+                      <Link to={stream.url} className="anime__data--link">
+                        {stream.name}
+                      </Link>
+                    ))}
+                  </div>
+                )}
+              </div>
+
+              <div className="anime__image">
+                <img
+                  src={anime.images.webp.large_image_url}
+                  alt={`${anime.title_english} cover`}
+                  className="anime__image--cover"
+                />
+                {anime.score && <p className="anime__score">{anime.score}</p>}
+              </div>
+            </div>
           </div>
-        )}
-
-        <div className="anime__image">
-          <img
-            src={anime.images.webp.large_image_url}
-            alt={`${anime.title_english} cover`}
-            className="anime__image--cover"
-          />
-          {anime.score && <p className="anime__score">{anime.score}</p>}
         </div>
       </div>
     </div>
